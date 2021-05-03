45758ae817db7c582b96427bb98d11eb6a130a6b
AleksKravtsov <sanksland@gmail.com>
2021-05-03 11:45:09 +0300

    Tasks for lesson_6

--- task_4.py
+++ task_4.py
@@ -1,28 +1,85 @@
-# Представлен список чисел. Определите элементы списка, не имеющие повторений.
-# Сформируйте итоговый массив чисел, соответствующих требованию.
-# Элементы выведите в порядке их следования в исходном списке.
-# Для выполнения задания обязательно используйте генератор.
-# Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
-# Результат: [23, 1, 3, 10, 4, 11]
+# Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево).
+# А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась, повернула (куда).
+# Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+# Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля.
+# Для классов TownCar и WorkCar переопределите метод show_speed.
+# При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+# Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
+# Выполните вызов методов и также покажите результат.
 
-'''from itertools import permutations
-my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-new_list = permutations(my_list, 3)
+class Car:
 
-print(*(new_list))'''
+    def __init__(self, name, color, speed, is_police=False):
+        self.name = name
+        self.color = color
+        self.speed = speed
+        self.is_police = is_police
+        print(f"New car: {self.name} (color {self.color}) police car - {self.is_police}")
 
-my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-# used = set()
-# unique = [x for x in my_list if x not in used and (used.add(x) or True)]
-# print(unique)
-my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-from collections import Counter
-counter = Counter(my_list)
-new_list = [x for x,n in counter.items() if n==1]
-print(new_list)
+    def go(self):
+        print(f"{self.name}: Car is going. ")
 
-# unique = list(counter)
-# print(unique)
+    def stop(self):
+        print(f"{self.name}: Car stopped. ")
 
-single = [x for x,n in counter.items() if n==1]
-print(single)
+    def turn(self, direction):
+        print(f"{self.name}: Car is turning {'left' if direction == 0 else 'right'}. ")
+
+    def show_speed(self):
+        print(f"{self.name}: Car`s speed: {self.speed}. ")
+
+class TownCar(Car):
+
+    def show_speed(self):
+        return f"{self.name}: Car`s speed: {self.speed}. Speeding!" if self.speed > 60 else f"{self.name}: Car`s speed: {self.speed}. "
+
+class WorkCar(Car):
+
+    def show_speed(self):
+        return f"{self.name}: Car`s speed: {self.speed}. Speeding!" if self.speed > 40 else f"{self.name}: Car`s speed: {self.speed}. "
+
+class SportCar(Car):
+    pass
+
+
+class PoliceCar(Car):
+    def __init__(self, name, color, speed, is_police=True):
+        super().__init__(name, color, speed, is_police)
+
+police_car = PoliceCar('Ford', 'black', 90)
+police_car.go()
+print(police_car.show_speed())
+police_car.turn(0)
+police_car.turn(0)
+police_car.stop()
+print()
+
+work_car = WorkCar('Kamaz', 'green', 50)
+work_car.go()
+work_car.turn(0)
+print(work_car.show_speed())
+work_car.turn(1)
+work_car.stop()
+print()
+
+sport_car = SportCar('Mazda', 'red', 160)
+sport_car.go()
+sport_car.turn(0)
+print(sport_car.show_speed())
+sport_car.turn(1)
+sport_car.stop()
+print()
+
+town_car = TownCar('Reno', 'wight', 70)
+town_car.go()
+town_car.turn(1)
+print(town_car.show_speed())
+town_car.turn(1)
+town_car.stop()
+
+
+
+
+
+
+
--- task_5.py
+++ task_5.py
@@ -1,11 +1,32 @@
-# Реализовать формирование списка, используя функцию range() и возможности генератора.
-# В список должны войти чётные числа от 100 до 1000 (включая границы).
-# Нужно получить результат вычисления произведения всех элементов списка.
-# Подсказка: использовать функцию reduce().
+# Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название) и метод draw (отрисовка).
+# Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер).
+# В каждом из классов реализовать переопределение метода draw. Для каждого из классов методы должен выводить уникальное сообщение.
+# Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 
-from functools import reduce
+class Stationery:
+    def __init__(self, title="Drawing things"):
+        self.title = title
 
-def my_f(a, b):
-    return a * b
-print(list(range(100, 1001, 2)))
-print(reduce(my_f, list(range(100, 1001, 2))))
+    def draw(self):
+        print(f"Start drawing. {self.title}")
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Start drawing with. {self.title} pen. ")
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Start drawing with. {self.title} pencil. ")
+
+class Marker(Stationery):
+    def draw(self):
+        print(f"Start drawing with. {self.title} marker. ")
+
+start = Stationery()
+start.draw()
+pen = Pen("Black")
+pen.draw()
+pencil = Pencil("Союз")
+pencil.draw()
+marker = Marker("Green")
+marker.draw()
